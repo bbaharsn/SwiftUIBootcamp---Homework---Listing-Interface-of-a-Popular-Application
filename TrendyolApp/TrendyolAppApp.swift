c50f8dd8c0872261595a05e96faa2ce4fcc01bfa
Bahar Şen <bahar_sen09@icloud.com>
2026-03-21 22:38:59 +0300

    Initial Commit

---
+++ TrendyolApp/TrendyolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendyolAppApp.swift
+//  TrendyolApp
+//
+//  Created by Bahar Şen on 21.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TrendyolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
